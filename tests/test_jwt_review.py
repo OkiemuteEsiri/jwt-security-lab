01e0814d58011852f6211707a5b5325c7f0ebd87
OkiemuteEsiri <okiemuteesiri37@gmail.com>
2026-09-09 04:54:29 +0100

    Fix dynamic module registration in JWT tests

--- tests/test_jwt_review.py
+++ tests/test_jwt_review.py
@@ -1,10 +1,12 @@
 import importlib.util
 import pathlib
+import sys
 import unittest
 
 MODULE = pathlib.Path(__file__).parents[1] / "src" / "jwt_review.py"
 spec = importlib.util.spec_from_file_location("jwt_review", MODULE)
 jwt_review = importlib.util.module_from_spec(spec)
+sys.modules[spec.name] = jwt_review
 spec.loader.exec_module(jwt_review)
 
 
